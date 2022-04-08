a1c1bf4f5820381271f288607ab4766a4e2a9b10
Jason Taylor <jasontaylor@ssw.com.au>
2022-04-08 10:39:28 +1000

    GettingStarted-03

---
+++ CaWorkshop/CaWorkshop.WebUI/Controllers/TodoItemsController.cs
@@ -0,0 +1,82 @@
+#nullable disable
+using CaWorkshop.WebUI.Data;
+using CaWorkshop.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaWorkshop.WebUI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoItemsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public TodoItemsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // PUT: api/TodoItems/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
+    {
+        if (id != todoItem.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(todoItem).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!TodoItemExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/TodoItems
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<int>> PostTodoItem(TodoItem todoItem)
+    {
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+
+        return todoItem.Id;
+    }
+
+    // DELETE: api/TodoItems/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(int id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        _context.TodoItems.Remove(todoItem);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool TodoItemExists(int id)
+    {
+        return _context.TodoItems.Any(e => e.Id == id);
+    }
+}
