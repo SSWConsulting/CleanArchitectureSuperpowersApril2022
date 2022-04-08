6986708a213fa665071adfce9f7cbc4426f1c8d7
Jason Taylor <jasontaylor@ssw.com.au>
2022-04-08 15:05:18 +1000

    BeyondBasics-05

--- CaWorkshop/CaWorkshop.Application/ConfigureServices.cs
+++ CaWorkshop/CaWorkshop.Application/ConfigureServices.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CaWorkshop.Application.Common.Behaviours;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace CaWorkshop.Application;
@@ -8,7 +10,11 @@ public static class ConfigureServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
--- CaWorkshop/CaWorkshop.Application/TodoLists/Commands/CreateTodoList/CreateTodoList.cs
+++ CaWorkshop/CaWorkshop.Application/TodoLists/Commands/CreateTodoList/CreateTodoList.cs
@@ -1,8 +1,35 @@
-﻿namespace CaWorkshop.Application.TodoLists.Commands.CreateTodoList;
+﻿using FluentValidation;
+
+namespace CaWorkshop.Application.TodoLists.Commands.CreateTodoList;
 
 public class CreateTodoListCommand : IRequest<int>
 {
     public string? Title { get; set; }
+}
+
+public class CreateTodoListCommandValidator
+    : AbstractValidator<CreateTodoListCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateTodoListCommandValidator(
+        IApplicationDbContext context)
+    {
+        _context = context;
+
+        RuleFor(v => v.Title)
+            .MaximumLength(240)
+            .NotEmpty()
+            .MustAsync(BeUniqueTitle)
+                .WithMessage("The specified title already exists.");
+    }
+
+    public async Task<bool> BeUniqueTitle(string title,
+        CancellationToken cancellationToken)
+    {
+        return await _context.TodoLists
+            .AllAsync(l => l.Title != title, cancellationToken);
+    }
 }
 
 public class CreateTodoListCommandHandler
